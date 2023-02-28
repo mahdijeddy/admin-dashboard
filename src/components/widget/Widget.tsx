162155e4006b17f70909d4c5f1db0693344c8e71
mahdi <mahdiproguni@gmail.com>
2023-02-28 17:48:05 +0330

    widget component created

---
+++ src/components/widget/Widget.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+    PersonOutlined,
+    ShoppingCartOutlined,
+    MonetizationOnOutlined,
+    AccountBalanceWalletOutlined,
+    KeyboardArrowUp,
+} from "@mui/icons-material";
+import "./widget.scss";
+
+interface propsType {
+    type: "user" | "order" | "earning" | "balance";
+}
+interface dataType {
+    title: string;
+    isMoney: boolean;
+    link: string;
+    icon: JSX.Element;
+}
+
+const Widget: React.FC<propsType> = ({ type }) => {
+    let data: dataType = {
+        title: "",
+        isMoney: false,
+        link: "",
+        icon: <div></div>,
+    };
+    const diff = 25;
+    switch (type) {
+        case "user":
+            data = {
+                title: "USERS",
+                isMoney: false,
+                link: "See all users",
+                icon: (
+                    <PersonOutlined
+                        className="icon"
+                        style={{
+                            color: "crimson",
+                            backgroundColor: "rgba(255, 0, 0, 0.2)",
+                        }}
+                    />
+                ),
+            };
+            break;
+        case "order":
+            data = {
+                title: "ORDERS",
+                isMoney: false,
+                link: "View all orders",
+                icon: (
+                    <ShoppingCartOutlined
+                        className="icon"
+                        style={{
+                            backgroundColor: "rgba(218, 165, 32, 0.2)",
+                            color: "goldenrod",
+                        }}
+                    />
+                ),
+            };
+            break;
+        case "earning":
+            data = {
+                title: "EARNINGS",
+                isMoney: true,
+                link: "View net earnings",
+                icon: (
+                    <MonetizationOnOutlined
+                        className="icon"
+                        style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+                    />
+                ),
+            };
+            break;
+        case "balance":
+            data = {
+                title: "BALANCE",
+                isMoney: true,
+                link: "See details",
+                icon: (
+                    <AccountBalanceWalletOutlined
+                        className="icon"
+                        style={{
+                            backgroundColor: "rgba(128, 0, 128, 0.2)",
+                            color: "purple",
+                        }}
+                    />
+                ),
+            };
+            break;
+        default:
+            break;
+    }
+    return (
+        <div className="widget">
+            <div className="left">
+                <span className="title">{data.title}</span>
+                <span className="counter">{data.isMoney && "$"}150</span>
+                <span className="link">{data.link}</span>
+            </div>
+            <div className="right">
+                <div className="percentage positive">
+                    <KeyboardArrowUp />
+                    {diff} %
+                </div>
+                {data.icon}
+            </div>
+        </div>
+    );
+};
+
+export default Widget;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
 function Home() {
@@ -9,6 +10,16 @@ function Home() {
             <Sidebar />
             <div className="homeContainer">
                 <Navbar />
+                <div className="Widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="balance" />
+                    <Widget type="earning" />
+                </div>
+                <div className="charts">charts</div>
+                <div className="listContainer">
+                    <div className="listTitle">Latest</div>
+                </div>
             </div>
         </div>
     );
